edc7d36262bb836874f13f23b2760ad7b1e04954
ccarl0 <carlo.cecco0@gmail.com>
2023-11-18 15:12:46 +0100

    Working demo bluetooth functionality

--- mattiBlue/mattiBlue/ViewModels/ContactsViewModel.cs
+++ mattiBlue/mattiBlue/ViewModels/ContactsViewModel.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Android.Database;
+using CommunityToolkit.Mvvm.ComponentModel;
 using mattiBlue.Models;
 
 namespace mattiBlue.ViewModels
 {
-    internal class ContactsViewModel
+    partial class ContactsViewModel : ObservableObject
     {
         
     }
--- mattiBlue/mattiBlue/Views/ContactsPage.xaml.cs
+++ mattiBlue/mattiBlue/Views/ContactsPage.xaml.cs
@@ -6,4 +6,29 @@ public partial class ContactsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var enable = new Android.Content.Intent(
+                    Android.Bluetooth.BluetoothAdapter.ActionRequestEnable);
+        enable.SetFlags(Android.Content.ActivityFlags.NewTask);
+
+        var disable = new Android.Content.Intent(
+                    Android.Bluetooth.BluetoothAdapter.ActionRequestDiscoverable);
+        disable.SetFlags(Android.Content.ActivityFlags.NewTask);
+
+        var bluetoothManager = (Android.Bluetooth.BluetoothManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.BluetoothService);
+        var bluetoothAdapter = bluetoothManager.Adapter;
+        if (bluetoothAdapter.IsEnabled == true)
+        {
+            Android.App.Application.Context.StartActivity(disable);
+            //  disable the bluetooth;
+        }
+        else
+        {
+            // enable the bluetooth
+            Android.App.Application.Context.StartActivity(enable);
+
+        }
+    }
 }
