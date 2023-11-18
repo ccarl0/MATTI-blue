b49ed77a47dfb2cba008fba23707c21537f0e8ee
ccarl0 <carlo.cecco0@gmail.com>
2023-11-18 11:32:31 +0100

    create file

---
+++ MATTIblue/MATTIblue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MATTIblue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MATTIblue/MATTIblue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MATTIblue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
